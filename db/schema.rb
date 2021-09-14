ed7f08214ab4c2c3e11865dc0341935429e53a20
meanstackBasit <basitkhanbasit640@gmail.com>
2021-09-14 20:17:56 +0500

    added a user reference in articles table added a debuger and font awesome gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_121418) do
+ActiveRecord::Schema.define(version: 2021_09_14_145825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_09_14_121418) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.text "body"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
